3e5e94de90a9b6c172aab4c25b4f46efc05d0c3c
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-02-08 09:37:56 -0600

    add digital to analog converter

---
+++ DAC.py
@@ -0,0 +1,15 @@
+import pyb 
+from pyb import DAC 
+import math 
+
+
+# Configure and Initialize Pin 'PA4' as analog
+pin = pyb.Pin('A4', pyb.Pin.ANALOG)
+
+
+# create a buffer containing a sine-wave, using half-word samples
+buf = array('H', 2048 + int(2047 * math.sin(2 * math.pi * i / 128)) for i in range(128))
+
+# output the sine-wave at 400Hz
+dac = DAC(pin, bits=12)
+dac.write_timed(buf, 400 * len(buf), mode=DAC.CIRCULAR)

06d1b49c432d0d828c26035263219264235bef0e
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-03-08 16:24:57 -0600

    move sine and square wave files

---
+++ signals/PWM_LED.py
@@ -0,0 +1,47 @@
+# main.py -- put your code here!
+import pyb 
+
+# Initialize pins
+# on the STM32F4DISC the main LEDS are connected to pins
+# PD12-PD15 having channesl 1-4 of timer 4 as the alternate
+# function #2
+led1 = pyb.Pin('D12')
+led2 = pyb.Pin('D13')
+led3 = pyb.Pin('D14')
+led4 = pyb.Pin('D15')
+led1.on()
+led2.on()
+led3.on()
+led4.on()
+
+# create an analog object from a pin
+pin = pyb.Pin('C1')
+adc = pyb.ADC(pin)
+
+# Create and initialize timer object at timer 4
+# Set up the timer so that it starts counting
+# We will configure it to count upwards with a period
+# of 500 and the prescaler set to 40000	
+timer = pyb.Timer(4, prescaler=40000, period=500)
+
+
+# We will now configure the timer to generate 80% impulses
+# on LED1 connected to D12 (i.e active 4/5 of the period and 
+# inactive for the next 1/5). This is done by configuring 
+# the channel 1 in the PWM mode with a compare value of 
+# 400 (out of period 500)
+ch1 = timer.channel(1, pyb.Timer.PWM, pin=led1, pulse_width=400)
+
+while True:
+
+	val = int(adc.read())
+    print(val)
+
+    # we can easily configure the timer to generate pulse signal on
+	# another channel. The period will have to be the same, however
+	# the compare value (pulse_width) can be different. 
+	# In this case we will configure the timer to generate 20% impulses
+	# on LED2 (i.e active 1/5 of the period and inactive for the next 4/5)
+	# This is done by configuring the channel 2 in the PWM mode with a compare 
+	# value of 100 (out of period 500)
+	ch2 = timer.channel(2, pyb.Timer.PWM, pin=led2, pulse_width=val)
---
+++ signals/square_wave.py
@@ -0,0 +1,23 @@
+# main.py -- put your code here!
+import pyb
+from pyb import DAC
+import math
+from array import array
+
+
+def sign(x): return math.copysign(1, x)
+
+# Configure and Initialize Pin 'PA4' as analog
+pin = pyb.Pin('A5', pyb.Pin.ANALOG)
+pin2 = pyb.Pin('A4', pyb.Pin.ANALOG)
+
+# create a buffer containing a square-wave, using half-word samples
+buf = array('H', 2048 + int(2047 * sign(math.sin(2 * math.pi * i / 128))) for i in range(128))
+
+# Initialize channel 1 and 2 of DAC
+dac = DAC(pin, bits=12)
+dac2 = DAC(pin2, bits=12)
+
+#dac.write_timed(buf, 800 * len(buf), mode=DAC.CIRCULAR)
+dac.write_timed(buf, pyb.Timer(6, freq=100), mode=DAC.CIRCULAR)
+dac2.write_timed(buf, pyb.Timer(6, freq=100), mode=DAC.CIRCULAR)

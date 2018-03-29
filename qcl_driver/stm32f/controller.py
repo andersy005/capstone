2bcd8c7260cf9784cb244cc337fcc6ae02f84b94
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-03-29 06:12:04 -0500

    add feedback loop functions

--- qcl_driver/stm32f/controller.py
+++ qcl_driver/stm32f/controller.py
@@ -37,7 +37,7 @@ def feedback_loop(qcl_current, shunt_resistor=0.05, gain=10, vref=3.):
 
 
 
-def is_safe(qcl_current=qcl_current, measured_current=measured_current, tolerance=0.1):
+def is_safe(qcl_current, measured_current, tolerance=0.1):
     if measured_current >= qcl_current-tolerance and measured_current <= qcl_current+tolerance:
         return True
 
@@ -45,78 +45,3 @@ def is_safe(qcl_current=qcl_current, measured_current=measured_current, toleranc
         return False 
 
 
-
-
-
-
-
-
-##########################################################################
-#                       Digital to Analog Converter (DAC)                #
-#                                                                        #
-##########################################################################
-
-POWER_MAX = 72.
-POWER_MIN = 65.
-LASER_VOLTAGE = 20.
-LASER_CURRENT = 3.6
-
-
-def sanitize_power_input(power):
-    """[summary]
-    
-    Arguments:
-        power {[type]} -- [description]
-    
-    Returns:
-        [type] -- [description]
-    """
-
-    # check lower and upper bound for the power
-    if (power >= POWER_MIN) and (power <= POWER_MAX):
-        return float(power)
-
-    else:
-        print('Input out of range. Setting the power to allowed minimum value of {}'.format(POWER_MIN))
-        return float(POWER_MIN)
-
-
-power = sanitize_power_input(70)
-print('Power = {} W'.format(power))
-
-
-def create_dac(power, dac_pin):
-    """[summary]
-    
-    Arguments:
-        power {[type]} -- [description]
-        dac_pin {[type]} -- [description]
-    """
-
-    current = power / LASER_VOLTAGE
-    dac_value = int((current / LASER_CURRENT) * 4095)
-    print(dac_pin)
-    print(current, dac_value)
-    dac = pyb.DAC(dac_pin, bits=12)
-    dac.write(dac_value)
-
-
-def set_laser_voltage(dac_pin):
-    """[summary]
-    
-    Arguments:
-        dac_pin {[type]} -- [description]
-    """
-
-    volt = LASER_VOLTAGE / 10.
-    dac_value = int(volt / 3. * 4095)
-    print(dac_pin)
-    print(volt, dac_value)
-    dac = pyb.DAC(dac_pin, bits=12)
-    dac.write(dac_value)
-
-dac_pin1 = init_pin('A5', mode=pyb.Pin.ANALOG)
-dac_pin2 = init_pin('A4', mode=pyb.Pin.ANALOG)
-
-create_dac(power, dac_pin1)
-set_laser_voltage(dac_pin2)
